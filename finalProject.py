747e645f7c8bcc04562dceb200783233d7162a13
Myron Phan <myron.phan@gmail.com>
2023-03-22 16:42:30 -0700

    added new test case

--- finalProject.py
+++ finalProject.py
@@ -1,7 +1,55 @@
-from trajectoryGeneration import *
-from feedback import *
-from nextSate import *
+from trajectoryGeneration import trajectoryGenerator
+from feedback import retrieveJointVelocities, feedback, calculateJacobianBody
+from nextSate import nextState
 import matplotlib.pyplot as plt
+import modern_robotics as mr
+import numpy as np
+
+pi = np.pi
+
+Slist = np.array([[0 , 0 , 1 , -300 , 0 , 0],
+                    [0 , 1 , 0 , -240 , 0 , 0],
+                    [0 , 1 , 0 , -240 , 0 , 244],
+                    [0 , 1 , 0 , -240 , 0 , 457],
+                    [0 , 0 , -1 , 169 , 457 , 0],
+                    [0 , 1 , 0 , -155 , 0 , 457]]).T
+
+M = np.array([[1, 0,  0, 457],
+                [ 0, 1,  0, 78],
+                [ 0, 0, 1, 155],
+                [ 0, 0,  0, 1]])
+
+Tse_initial = np.array([[0, 1, 0, 247],
+                    [1, 0, 0, -169],
+                    [0, 0, -1, 782],
+                    [0, 0, 0, 1]])
+
+# Tse_initial = np.array([[0, 0, 1, 323.5756],
+#                     [-1, 0, 0, -335.5507],
+#                     [0, -1, 0, 237.0000],
+#                     [0, 0, 0, 1]])
+
+Tsc_initial = np.array([[1, 0, 0, 450],
+                    [0, 1, 0, -300],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+
+Tsc_final = np.array([[0, -1, 0, 0],
+                    [1, 0, 0, 100],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+
+Tce_grasp = np.array([[0, 0, 1, 0],
+                    [-1, 0, 0, 0],
+                    [0, -1, 0, 20],
+                    [0, 0, 0, 1]])
+
+Tce_standoff = np.array([[0, 0, 1, 0],
+                    [-1, 0, 0, 0],
+                    [0, -1, 0, 70],
+                    [0, 0, 0, 1]])
+
+N = 251
 
 # max velocities
 max_velos = np.array([10000*pi,10000*pi,10000*pi,10000*pi,10000*pi,10000*pi])
@@ -17,38 +65,74 @@ Tse_trajectory = np.zeros((pose_trajectory.shape[0],4,4))
 ErrorTwist_trajectory = np.zeros((pose_trajectory.shape[0]-1,6))
 Tse_trajectory[0,...] = Tse
 ErrorTwist_trajectory[0,...] = np.zeros((6,))
-error_trajactory = np.zeros((pose_trajectory.shape[0]-1,3))
+error_trajectory = np.zeros((pose_trajectory.shape[0]-1,3))
+
+dt = 0.01
 
 # initial joint angles
-position = np.array([-pi/6,-pi/2,pi/2,-pi/2,-pi/2,5*pi/6])
 
-# control grains
-kp = 60
-ki = 50
+# generated from inverseKinematics.py
+position = np.array([0.0012750106140693163, 4.711381176104732, 0.0029464399820895437, 4.710450344682556, 4.711113969770621, 0.0])
+# position = np.array([-pi/6, -pi/2, pi/2, -pi/2, -pi/2, 5 * pi/6])
+position_trajectory = np.zeros((pose_trajectory.shape[0],6))
+position_trajectory[0,:]=position
 
-for i in range(1499):
-    # desired trajectories
-    Tse_d = pose_trajectory[i,...]
-    Tse_d_next = pose_trajectory[i+1,...]
 
-    # calculate error twist, end effector jacobian and then joint velocities at time i
-    TwistEndEffector = feedback(Tse, Tse_d, Tse_d_next, kp, ki, dt)
-    JacobianEndEffector = calculateJacobianBody(Slist, position, Tse)
-    JointVelocities = retrieveJointVelocities(JacobianEndEffector, TwistEndEffector)
+MODE = 0
+if MODE == 0: 
+    kp = 1
+    ki = 1
+elif MODE == 1:
+    kp = 1000
+    ki = 50
+elif MODE == 2:
+    kp = 60
+    ki = 50
+else:
+    kp = 60
+    ki = 50
+
+def best_case_1():
+    pass
+
+def overshoot_case_2():
+    pass
+
+def new_task_case_3():
+    pass 
+
+def finalProject(Tse, position, position_trajectory, error_trajectory, ErrorTwist_trajectory):
+    for i in range(pose_trajectory.shape[0]-1):
+        # desired trajectories
+        Tse_d = pose_trajectory[i,...]
+        Tse_d_next = pose_trajectory[i+1,...]
+
+        # calculate error twist, end effector jacobian and then joint velocities at time i
+        TwistEndEffector = feedback(Tse, Tse_d, Tse_d_next, kp, ki, dt)
+        JacobianEndEffector = calculateJacobianBody(Slist, position, Tse)
+        JointVelocities = retrieveJointVelocities(JacobianEndEffector, TwistEndEffector)
+        
+        # calculate next state and compute forward dynamics for new configuration
+        position = nextState(position, JointVelocities, dt, max_velos)
+        position_trajectory[i+1,:] = position
+        Tse = mr.FKinSpace(M, Slist, position)
+        
+        # log Tse and error twist into trajectory
+        Tse_trajectory[i+1,...] = Tse
+        ErrorTwist_trajectory[i,...] = TwistEndEffector
+        
+        error = np.abs(np.linalg.norm(Tse_d_next[:3,3]-Tse[:3,3]))
+        error_trajectory[i,:] = error
+        if i % 50 == 0:
+            pass
+
+    plt.figure(1)
+    plt.plot(error_trajectory)
+    plt.show()
     
-    # calculate next state and compute forward dynamics for new configuration
-    position = nextState(position, JointVelocities, dt, max_velos)
-    Tse = mr.FKinSpace(M, Slist, position)
+    data = np.hstack((position_trajectory, gripper_trajectory[:,None]))
     
-    # log Tse and error twist into trajectory
-    Tse_trajectory[i+1,...] = Tse
-    ErrorTwist_trajectory[i,...] = TwistEndEffector
+    np.savetxt("finalProject.csv", data, delimiter=",",fmt="%.2f")
     
-    error = np.abs(np.linalg.norm(Tse_d_next[:3,3]-Tse[:3,3]))
-    error_trajactory[i,:] = error
-
-plt.figure(1)
-plt.plot(error_trajactory)
-plt.show()
-
-print("hi")
+print("u the goat")
+finalProject(Tse, position, position_trajectory, error_trajectory, ErrorTwist_trajectory)
